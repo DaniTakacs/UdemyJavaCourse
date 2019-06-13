cc02d7b46c68ac6ddc63a38b7d3129d2c684f5f9
Dániel Takács <takacs.d24@gmail.com>
2019-06-13 15:32:00 +0200

    Reading User Input

--- ReadingUserInput/src/com/danitakacs/Main.java
+++ ReadingUserInput/src/com/danitakacs/Main.java
@@ -1,8 +1,22 @@
 package com.danitakacs;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth:");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine();
+        int age = 2019 - yearOfBirth;
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Your name is " + name + " and you are " + age + " years old.");
+
+        scanner.close();
     }
 }
